a65ae5546d3baa5613ec060738ad0669cf6a1d23
Mindsterz <43070071+Mindsterz@users.noreply.github.com>
2024-02-24 11:24:54 +0100

    Update exo8.1.py
    Ajout de commentaire

--- exo8.1.py
+++ exo8.1.py
@@ -2,11 +2,11 @@ def saisieEntier() :
     
     x = int(input("Longueur de la suite : "))
     L = []
-    y = int(input("Entrez un entier : "))
-    L.append(y)
+    y = int(input("Entrez un entier : ")) #Saisi un entier
+    L.append(y)                           #Puis l'ajoute à la liste
     x = x - 1
 
-    while x > 0 :
+    while x > 0 : #Boucle jusqu'à que la liste fasse la bonne longueur
 
         y = int(input("Entrez un entier : "))
         L.append(y)
@@ -22,9 +22,9 @@ def valRemarquable(L) :
     valmin = L[0]
     valmax = L[0]
 
-    for i in range(len(L)) :
+    for i in range(len(L)) : #Parcours toute la liste
 
-        somme = L[i] + somme
+        somme = L[i] + somme #Fait la somme de chaque entier
 
         if valmin > L[i] :
             valmin = L[i]
@@ -36,7 +36,7 @@ def valRemarquable(L) :
         else :
             valmax = valmax
     
-    moyenne = somme / len(L)
+    moyenne = somme / len(L) #Calcul la moyenne grâceà la fonction len qui indique la longueur (nombre d'entier dans la liste) de la liste
 
     return moyenne,valmin,valmax
 
@@ -47,4 +47,4 @@ moyenne,min,max = valRemarquable(L)
 
 print("Liste =",L)
 
-print("Min =", min,", max =", max,",moyenne =",round(moyenne, 2))
+print("Min =", min,", max =", max,",moyenne =",round(moyenne, 2)) #On arrondit la moyenne histoire d'avoir des valeurs pas trop moche
